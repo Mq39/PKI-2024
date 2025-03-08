5ea63a28b753d6500c233febf961226b929de7aa
Mq39 <predragmitr@gmail.com>
2025-03-08 17:36:12 +0100

    Added flight form on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,30 @@ import { RouterLink } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { PageModel } from '../../models/page.model';
 import { FlightModel } from '../../models/flight.model';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [MatCardModule, MatButtonModule, HttpClientModule, NgIf, NgFor, RouterLink, MatListModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    NgIf,
+    NgFor,
+    RouterLink,
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
 })
 export class HomeComponent {
   public recommended: FlightModel[] = [];
+  public destinations: string[] = ['Zagreb', 'Memmingen', 'Vienna'];
+  public airlines: string[] = ['Air Serbia', 'Fly Emirates', 'Lufthansa'];
+  public flightClasses: string[] = ['First class', 'Business', 'Economy'];
 
   constructor(client: HttpClient) {
     const url = 'https://flight.pequla.com/api/flight?page=0&size=3&type=departure&sort=scheduledAt,desc';
