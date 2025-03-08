43933f950cc4d0d68c7c0e2f694ec189c895db78
Mq39 <predragmitr@gmail.com>
2025-03-08 16:06:32 +0100

    Added https requests

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,36 @@
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule, JsonpClientBackend } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [MatCardModule, MatButtonModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatCardModule, MatButtonModule, HttpClientModule, NgIf, NgFor, RouterLink, MatListModule],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public recommended: any[] = [];
+
+  constructor(client: HttpClient) {
+    const url = 'https://flight.pequla.com/api/flight?page=0&size=3&type=departure&sort=scheduledAt,desc';
+    client
+      .get<any>(url, {
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+      .subscribe((rsp) => (this.recommended = rsp.content));
+  }
+
+  public generateImageUrl(dest: string) {
+    return `https://img.pequla.com/destination/${dest.split(' ')[0].toLowerCase()}.jpg`;
+  }
+
+  public formatDate(iso: string) {
+    return new Date(iso).toLocaleString('sr-RS');
+  }
+}
